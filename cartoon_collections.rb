7da1958e954f193e728c264ad66199182529ac09
Darin Webster <darinleenavy@gmail.com>
2019-07-04 00:09:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,45 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves = ["Dopey", "Grumpy", "Bashful"]
+  
+  dwarves.each_with_index do |name, index|
+  
+  puts "#{index + 1 } #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(fruits)
+  fruits = ["apple", "banana", "orange"]
+  
+  fruits.map { |item| item.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+  calls_long = ["axe", "earth", "wind", "fire"]
+  
+  if calls_long.length = (0..3) then
+    puts true
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def long_planeteer_calls(calls_short)
+  calls_short = ["axe", "earth", "wind", "fire"]
+  
+  if calls_short.length < 4 then
+    puts 
+  end
+end
+
+def find_the_cheese(contains_cheddar) 
+  contains_cheddar = ["cheddar", "gouda", "camembert"]
+  
+  contains_cheddar.include?("cheddar")
+  p cheddar
+  
+end
+
+def find_the_cheese(no_cheese)
+  no_cheese = ["ham", "cellphone", "computer"]
+  
+  no_cheese.include?("ham")
+  p nil
 end
